cce17003f7020d203ce38cc3cbf7794b01e48311
Unknown <dinhquochuy.eeit@gmail.com>
2018-03-09 13:57:17 +0700

    Working wrapper + alarm

--- Sensor_Display_Blynk_WithConfig_Proof.ino
+++ Sensor_Display_Blynk_WithConfig_Proof.ino
@@ -31,6 +31,162 @@ char auth[] = "439c6c8b9e6b43089fbba071d3d1983e";
 char ssid[] = "Phong An";
 char pass[] = "28091963";
 
+class BlynkWrapper
+{
+  private:
+    const static unsigned long EMAIL_INTERVAL = 120000;
+    bool lastTempAlarm;
+    bool lastHumidAlarm;
+    unsigned long lastTempEmailTime;
+    unsigned long lastHumidEmailTime;
+    float tempAlarmLevel;
+    float humidityAlarmLevel;
+    bool tempAlarmEnabled;
+    bool humidityAlarmEnabled;
+    
+    void notifyTempOutOfRange()
+    {
+      Blynk.notify("Temperature is out of range"); 
+    }
+    void notifyHumidOutOfRange()
+    {
+      Blynk.notify("Humidity is out of range");  
+    }
+    void emailTempOutOfRange()
+    {
+      Blynk.email("Server room monitoring alarm", "The temperature is currently out of range");  
+    }
+    void emailHumidOutOfRange()
+    {
+      Blynk.email("Server room monitoring alarm", "The humidity is currently out of range");  
+    }
+    void emailBothOutOfRange()
+    {
+      Blynk.email("Server room monitoring alarm", "The humidity is currently out of range");
+    }
+  public:
+    void init(char* auth_in, char* ssid_in, char* pass_in)
+    {
+      Blynk.begin(auth_in, ssid_in, pass_in);
+      lastTempAlarm = false;
+      lastHumidAlarm = false;
+      lastTempEmailTime = millis();
+      lastHumidEmailTime = millis();
+      tempAlarmEnabled = true;
+      tempAlarmLevel = 33;
+      humidityAlarmEnabled = true;
+      humidityAlarmLevel = 50;
+    }
+    
+    void updateTempAlarmLevel(float inputLevel)
+    {
+      tempAlarmLevel = inputLevel;
+    }
+    void updateTempAlarmEnabled(bool input)
+    {
+      tempAlarmEnabled = input;
+    }
+    void updateHumidityAlarmLevel(float inputLevel)
+    {
+      humidityAlarmLevel = inputLevel;
+    }
+    void updateHumidityAlarmEnabled(bool input)
+    {
+      humidityAlarmEnabled = input;
+    }
+    
+    void serviceAlarm(float t, float h)
+    {
+      if (t > tempAlarmLevel && tempAlarmEnabled)
+      {
+        if (lastTempAlarm)
+        {
+          // Do nothing
+        }
+        else
+        {
+          Serial.println("Temperature alarm");
+          notifyTempOutOfRange();
+          lastTempAlarm = true;
+          if (millis() - lastTempEmailTime > EMAIL_INTERVAL)
+          {
+            emailTempOutOfRange();
+            lastTempEmailTime = millis();
+          }
+        }
+      }
+      else
+      {
+        lastTempAlarm = false;
+      }
+      
+      if (h > humidityAlarmLevel && humidityAlarmEnabled)
+      {
+        if (lastHumidAlarm)
+        {
+          
+        }
+        else
+        {
+          Serial.println("Humidity alarm");
+          notifyHumidOutOfRange();
+          lastHumidAlarm = true;
+          if (millis() - lastHumidEmailTime > EMAIL_INTERVAL)
+          {
+            emailHumidOutOfRange();
+            lastHumidEmailTime = millis();
+          }
+        }
+      }
+      else
+      {
+        lastHumidAlarm = false;
+      }
+    }
+    
+    void updateTempAndHumid(float temp, float humid)
+    {
+      Blynk.virtualWrite(V0, temp);
+      Blynk.virtualWrite(V1, humid);
+    }
+    
+    void run()
+    {
+      Blynk.run();
+    }
+};
+
+BlynkWrapper ourWrapper;
+
+BLYNK_WRITE(V2)
+{
+  Serial.print("Update temp alarm level ");
+  Serial.println(param.asFloat());
+  ourWrapper.updateTempAlarmLevel(param.asFloat());
+}
+
+BLYNK_WRITE(V3)
+{
+  Serial.print("Update humid alarm level ");
+  Serial.println(param.asFloat());
+  ourWrapper.updateHumidityAlarmLevel(param.asFloat());
+}
+
+BLYNK_WRITE(V4)
+{
+  Serial.print("Update temp alarm enabled ");
+  Serial.println(param.asInt());
+  ourWrapper.updateTempAlarmEnabled((bool) param.asInt());
+}
+
+BLYNK_WRITE(V5)
+{
+  Serial.print("Update humid alarm enabled ");
+  Serial.println(param.asInt());
+  ourWrapper.updateHumidityAlarmEnabled((bool) param.asInt());
+}
+
+
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(2000);
@@ -51,12 +207,14 @@ void setup() {
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.drawString(0, 16, "Xin chao Udo");
   display.display();
-  Blynk.begin(auth, ssid, pass);
+  //Blynk.begin(auth, ssid, pass);
+  ourWrapper.init(auth, ssid, pass);
 }
 
 int timeSinceLastRead = 0;
 void loop() {
-  Blynk.run();
+  //Blynk.run();
+  ourWrapper.run();
   // Report every 2 seconds.
   if(timeSinceLastRead > 2000) {
     display.clear();
@@ -97,12 +255,8 @@ void loop() {
     Serial.println(displayString);
     display.drawString(0, 16, displayString);  
     display.display();
-    Blynk.virtualWrite(V0, t);
-    Blynk.virtualWrite(V1, h);
-    if (t > 32)
-    {
-      Blynk.notify("Udo trym be");  
-    }
+    ourWrapper.serviceAlarm(t, h);
+    ourWrapper.updateTempAndHumid(t, h);
     timeSinceLastRead = 0;
   }
   delay(10);
